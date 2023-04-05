33cf0e72687d03575b0ec85548a01c3ed810bf79
Marktapus <markjackson956@gmail.com>
2023-04-05 18:32:49 -0400

    Cleaned up for loop with console logs of winner

--- js/index.js
+++ js/index.js
@@ -43,7 +43,6 @@ let choices = ["rock", "paper", "scissors"];
 let rock = choices[0];
 let paper = choices[1];
 let scissors = choices[2];
-let totalRounds = 5;
 let userPoints = 0;
 let cpuPoints = 0;
 
@@ -54,22 +53,30 @@ function getComputerChoice() {
 }
 
 
-function playRound() {
-    for(let i = 0; i <= totalRounds; i++) {
 
+
+function playRound() {
+
+    for(let i = 0; i < 1; i++) {
+        let computerSelection = getComputerChoice();
         let userAnswer = prompt("Please select: Rock, Paper or Scissors (enter Rock, Paper or Scissors):");
         
-        if(userAnswer == "rock" && getComputerChoice() == "scissors" || userAnswer == "paper" && getComputerChoice() == "rock" || userAnswer == "scissors" && getComputerChoice() == "paper") {
+        if(userAnswer == "rock" && computerSelection == "scissors" || userAnswer == "paper" && computerSelection == "rock" || userAnswer == "scissors" && computerSelection == "paper") {
             userPoints++;
+            console.log("You Win! " + userAnswer + " beats " + computerSelection)
             console.log("Player Score: " + userPoints);
         }
-        else if(userAnswer == "rock" && getComputerChoice() == "paper" || userAnswer == "paper" && getComputerChoice() == "scissors" || userAnswer == "scissors" && getComputerChoice() == "rock") {
+        else if(userAnswer == "rock" && computerSelection == "paper" || userAnswer == "paper" && computerSelection == "scissors" || userAnswer == "scissors" && computerSelection == "rock") {
             cpuPoints++;
+            console.log("You Lose! " + computerSelection + " beats " + userAnswer)
             console.log("Computer Score: " + cpuPoints);
         }
         else {
-            totalRounds--;
+            i--;
             console.log("You both picked the same thing, try again")
         }
     }
 }
+
+function game(playRound)
+    for(let i = 0; i == 5; i++)
