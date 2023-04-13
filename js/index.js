9ae303c04e02910748331c1c9b079e9ae29ffbdb
Marktapus <markjackson956@gmail.com>
2023-04-12 22:25:41 -0400

    Add function to accept case insensetive choice

--- js/index.js
+++ js/index.js
@@ -1,39 +1,47 @@
 //Add a spoon option, but you can only use it if you say "Knife", the CPU states "thats not a knife, thats a spoon and implodes"
 
 let choices = ["rock", "paper", "scissors"];
-let rock = choices[0];
-let paper = choices[1];
-let scissors = choices[2];
 let userPoints = 0;
 let cpuPoints = 0;
+let totalRounds = 5;
 
 //Random answer for CPU, between rock, paper or scissors from array.
 function getComputerChoice() {
-    let randomNum = Math.floor(Math.random() * 3);
-    let cpuAnswer = choices[(randomNum)];
+    let cpuAnswer = choices[Math.floor(Math.random() * 3)];
     return cpuAnswer;
 }
 
-//Function for one single round with a user input and computer input,
+//Function on button to reload page
+function resetGame(){
+    document.getElementById(myGame).reset();
+} 
+
+
+//Function for a single round with a user input and computer input,
 function playRound() {
         let userAnswer = prompt("Please select: Rock, Paper or Scissors (enter Rock, Paper or Scissors):");
+        userAnswer = userAnswer.toLowerCase();
         let computerSelection = getComputerChoice();
+        //If user wins
         if(userAnswer == "rock" && computerSelection == "scissors" || userAnswer == "paper" && computerSelection == "rock" || userAnswer == "scissors" && computerSelection == "paper") {
             userPoints++;
             console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
             return userWin = "You Win! " + userAnswer + " beats " + computerSelection;
-            i++;
         }
+        //If cpu wins
         else if(userAnswer == "rock" && computerSelection == "paper" || userAnswer == "paper" && computerSelection == "scissors" || userAnswer == "scissors" && computerSelection == "rock") {
             cpuPoints++;
             console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
             return cpuWin = "You Lose! " + computerSelection + " beats " + userAnswer
-            i++;
         }
-        else {
+        //If user and cpu pick the same thing
+        else if (userAnswer == computerSelection){
             console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
             console.log("You both picked the same thing, try again")
-            i++;
+        }
+        //If invalid selection
+        else {
+            console.log("Invalid selection, try again")
         }
     }
 
@@ -41,18 +49,18 @@ function playRound() {
 //Function that invokes the playRound function and iterates through 5 rounds of rock paper scissors
 function game(oneRound) {
     oneRound = playRound()
-    for(let i = 0; i <= 5; i++) {
-        if(cpuPoints >= 5 && userPoints < 5) {
-            console.log("You lose sucker");
-            i = i+5;
+    while(userPoints < totalRounds || cpuPoints < totalRounds) {
+        if(userPoints == totalRounds) {
+            console.log("Congratulations you win!")
+            break;
         }
-        else if(userPoints >= 5 && cpuPoints < 5) {
-            console.log("Congratulations you win!");
-            i = i+5;
+        else if(cpuPoints == totalRounds) {
+            console.log("You lose sucker");
+             break;
         }
         else {
                 playRound()
         }
-
     }
 }
+//
