74b9ef729e01a1480aeca462c213154c0303d228
Mark Jackson <markjackson956@gmail.com>
2023-05-11 14:32:21 -0400

    test

--- js/index.js
+++ js/index.js
@@ -63,21 +63,3 @@ function game(oneRound) {
         }
     }
 }
-// //
-
-// for(let i = 2; i <= 10; i++) {
-//     if(i % 2 == 0) {
-//         console.log(i);
-//     }
-// }
-
-// for (let i = 0; i < 3; i++) {
-//     alert( `number ${i}!` );
-//   }
-
-//   let i = 0;
-//   while(i < 3) {
-
-//     alert( `number ${i}!` );
-//     i++
-//   }
