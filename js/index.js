541d83e609d3da37aed8afbd5b908f35e3720736
Mark Jackson <markjackson956@gmail.com>
2023-08-07 20:46:44 -0400

    Add GUI buttons for Rock Paper and Scissors

--- js/index.js
+++ js/index.js
@@ -1,66 +1,83 @@
 //Add a spoon option, but you can only use it if you say "Knife", the CPU states "thats not a knife, thats a spoon and implodes"
 
-let choices = ["rock", "paper", "scissors"];
+
 let userPoints = 0;
 let cpuPoints = 0;
 let totalRounds = 5;
+let buttons = document.querySelectorAll('input')
 
 //Random answer for CPU, between rock, paper or scissors from array.
-function getComputerChoice() {
-    let cpuAnswer = choices[Math.floor(Math.random() * 3)];
-    return cpuAnswer;
+function computerPlay() {
+    let choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
-
-//Function on button to reload page
-function resetGame(){
-    document.getElementById(myGame).reset();
-} 
-
 
 //Function for a single round with a user input and computer input,
-function playRound() {
-        let userAnswer = prompt("Please select: Rock, Paper or Scissors (enter Rock, Paper or Scissors):");
-        userAnswer = userAnswer.toLowerCase();
-        let computerSelection = getComputerChoice();
-        //If user wins
-        if(userAnswer == "rock" && computerSelection == "scissors" || userAnswer == "paper" && computerSelection == "rock" || userAnswer == "scissors" && computerSelection == "paper") {
+function playRound(playerSelection) {
+
+    let computerSelection = computerPlay()
+    let result = ""
+
+        if(playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
             userPoints++;
-            console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
-            return userWin = "You Win! " + userAnswer + " beats " + computerSelection;
-        }
+            result = ("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints) + "You Win! " + playerSelection + " beats " + computerSelection;
+
+          if(userPoints == 5) {
+            result = ("You win!!!");
+              disableButtons();
+        }        
+      }
         //If cpu wins
-        else if(userAnswer == "rock" && computerSelection == "paper" || userAnswer == "paper" && computerSelection == "scissors" || userAnswer == "scissors" && computerSelection == "rock") {
+        else if(playerSelection == "rock" && computerSelection == "paper" || playerSelection == "paper" && computerSelection == "scissors" || playerSelection == "scissors" && computerSelection == "rock") {
             cpuPoints++;
-            console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
-            return cpuWin = "You Lose! " + computerSelection + " beats " + userAnswer
+            result = "Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints + "You Lose! " + computerSelection + " beats " + playerSelection;
+            
+            if(cpuPoints == 5) {
+              result = ("You lose sucker");
+                disableButtons();
+          }
         }
         //If user and cpu pick the same thing
-        else if (userAnswer == computerSelection){
-            console.log("Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints);
-            console.log("You both picked the same thing, try again")
+        else if (playerSelection == computerSelection){
+            result = "Your Score " + userPoints + " vs " + "CPU Score " + cpuPoints + "You both picked the same thing, try again";
         }
-        //If invalid selection
         else {
-            console.log("Invalid selection, try again")
         }
+        document.getElementById('result').innerHTML = result
+        return
     }
 
+function disableButtons() {
+  buttons.forEach(elem => {
+    elem.disabled = true
+  })
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', function(){
+    playRound(button.value)
+  })
+});
 
 //Function that invokes the playRound function and iterates through 5 rounds of rock paper scissors
-function game(oneRound) {
-    oneRound = playRound()
-    while(userPoints < totalRounds || cpuPoints < totalRounds) {
-        if(userPoints == totalRounds) {
-            console.log("Congratulations you win!")
-            break;
-        }
-        else if(cpuPoints == totalRounds) {
-            console.log("You lose sucker");
-             break;
-        }
-        else {
-                playRound()
-        }
-    }
-}
-// //
+// function game(oneRound) {
+//     oneRound = playRound()
+//     while(userPoints < totalRounds || cpuPoints < totalRounds) {
+//         if(userPoints == totalRounds) {
+//             console.log("Congratulations you win!")
+//             break;
+//         }
+//         else if(cpuPoints == totalRounds) {
+//             console.log("You lose sucker");
+//              break;
+//         }
+//         else {
+//                 playRound()
+//         }
+//     }
+// }
+
+//Function on button to reload page
+// function resetGame(){
+//   document.getElementById(myGame).reset();
+// }
